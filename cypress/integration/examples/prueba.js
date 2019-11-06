1bfd86e4b4fcf47a1228dd5159da069e8c7fd9f8
Hectorolon <hrolon@ucol.mx>
2019-11-06 07:03:06 -0600

    Pruebas hechas

--- cypress/integration/examples/prueba.js
+++ cypress/integration/examples/prueba.js
@@ -1,14 +1,15 @@
-describe('Navegando por el sitio de la Universidad de Colima',function() {
- it('Navegación por portal ucol', function() {
- cy.visit('https://www.ucol.mx')
- })
- it('Búsqueda en portal ucol', function() {
- cy.get('#q').click()
- cy.get('#q')
- .type('plataforma educ').should('have.value',
-'plataforma educ')
-cy.get('.form-search-ucol').submit()
-cy.contains('menos basura en colima').click()
- })
+describe('Navegando por el sitio',function() {
+    it('Navegación', function() {
+        cy.visit('http://practica2-v.herokuapp.com/')
+    })
+    it('Campo fecha no deberia existir #1', function() {
+        cy.get('table').contains('tr', 'Fecha').should('not.exist')
+    })
+    it('Campo telefono debe existir #2', function() {
+        cy.get('table').contains('tr', 'Teléfono').should('exist')
+    })
+    it('Borrar registro #3', function() {
+        cy.get('table').contains('Borrar').click()
+    })
 
 })
